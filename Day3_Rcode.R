c45d9d6c5df70524aeeb5ca8925b4a8e70e5c15f
jianli4 <dirichlet02@gmail.com>
2019-01-17 18:11:56 -0600

    update the program by adding an example of iris

--- Day3_Rcode.R
+++ Day3_Rcode.R
@@ -8,8 +8,26 @@ library(purrr)
 
 data<- iris %>% group_by(Species) %>% nest()
 fun<- function(x){
-  x[1]+x[2]
+ x[1]+x[2]
 }
 
-data2<- data %>% mutate(model=map(data,fun)) %>% unnest()
+fun2<- function(x){
+  return(x)
+}
+data2<- data %>% mutate(calc=map(data,fun)) %>% transmute(Species,res=map(calc,fun2)) %>% unnest()
 data2
+
+
+
+###examples
+n_iris<- iris %>% group_by(Species) %>% nest()
+mod_fun<- function(df) lm(Sepal.Length~.,data=df)
+m_iris<- n_iris %>% mutate(model=map(data,mod_fun))
+b_fun<- function(mod) coefficients(mod)[[1]]
+m_iris %>% transmute(Species,beta=map_dbl(model,b_fun))
+
+###nested dataframe 
+###which is also called as list column
+###can be created by purrr nest() function and dplyr and tibble packages
+
+
